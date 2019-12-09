20ee7b98e073af7eb9d15847f5f18329a28d07d8
trang <letth.trang@gmail.com>
2019-12-09 15:56:30 +0700

    get all category and product detail

--- app.py
+++ app.py
@@ -1,39 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 import json
+from object_data import product, category
+import psycopg2
+import app
+import pandas as pd
+from flask_menu import Menu, register_menu
 
-app = Flask(__name__)
 
-f = open("tiki_data","r")
-data = json.load(f)
-# print(data)
-
-all_products = []
-all_categories = []
-
-data_category = data['category']
-data_product = data['product']
-
-for item in data_category:
-    for i in range(len(data_category[item])):
-      if(i >= len(all_categories)):
-        temp = dict()
-        temp[item] = data_category[item][i]
-        all_categories.append(temp)
-      else:
-        all_categories[i][item] = data_category[item][i] 
-
-for item in data_product:
-    for i in range(len(data_product[item])):
-      if(i >= len(all_products)):
-        temp = dict()
-        temp[item] = data_product[item][i]
-        all_products.append(temp)
-      else:
-        all_products[i][item] = data_product[item][i] 
+conn = psycopg2.connect("dbname=postgres port=5433 user=postgres")
+data_category = category.category().get_parent_category(conn)
+app = Flask(__name__, template_folder="templates")
+Menu(app=app)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html', products = all_products, categories = all_categories)
+    data_product = product.product().get_all_product_by_category(data_category[0][0],conn)
+    return render_template('index.html', products = data_product, categories = data_category)
+
+@app.route('/product/<int:category_id>', methods=['GET','POST'])
+def get_product(category_id):
+    data_product = product.product().get_all_product_by_category((category_id,),conn)
+    return render_template('index.html', products = data_product, categories = data_category)
+
+@app.route('/categories/<int:category_id>', methods=['GET'])
+def get_child_category(category_id):
+    data_product = product.product().get_all_product_by_category((category_id,), conn)
+    child_categories = category.category().get_child_category((category_id,), conn)    
+    return render_template('index.html', parent_id = category_id, products = data_product, categories = data_category, child_categories = child_categories)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
