277848ec4da1c566137517a9f7a2172d889f3aca
trang <letth.trang@gmail.com>
2019-12-29 19:05:53 +0700

    Revert "clean folter"
    This reverts commit d8cbc4883c82d4816fed825d251a83f7b7ebe001.

---
+++ load_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from  extract_data_tiki import extract_all_category, extract_all_products_from_category, extract_category_detail
+import psycopg2, unicodedata
+import sys
+
+conn = psycopg2.connect("dbname=postgres port=5433 user=postgres")
+url_category = "https://tiki.vn/"
+
+
+if __name__ == "__main__":
+  # extract_all_category(url_category, conn)
+  ### Get product detail base on product_link
+  # extract_all_products_from_category(1846,conn)
+  # extract_category_detail("nha_sach_tiki","https://tiki.vn/nha-sach-tiki/c8322?src=c.8322.hamburger_menu_fly_out_banner",conn)
+  conn.close()
+
