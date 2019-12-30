f67319216117f782c61797ada2cb3564ae0e4fa6
trang <letth.trang@gmail.com>
2019-12-30 16:18:01 +0700

    clean repo

--- packages/common_libs/common.py
+++
@@ -1,8 +0,0 @@
-import unicodedata
-import pandas as pd
-
-def strip_accents(text):
-  # special case D stroke Latin: 
-  text = str(text).replace("đ","d")
-  text = unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
-  return str(text)
